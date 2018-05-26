bef045a0495f5750598dc14f8085a5c21e736b4a
Ivan <silver2006@tut.by>
2018-05-26 20:21:09 +0300

    chore(log4j2.properties) Configure log4j2

--- src/main/java/com/kuliashou/shape/action/ActionTriangle.java
+++ src/main/java/com/kuliashou/shape/action/ActionTriangle.java
@@ -1,9 +1,26 @@
 package com.kuliashou.shape.action;
 
+import com.kuliashou.shape.entity.Triangle;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ActionTriangle {
 
     private static final Logger logger = LogManager.getLogger();
+
+    public float calculateArea(Triangle triangle) {
+        return 1;
+    }
+
+    public float calculatePerimeter(Triangle triangle) {
+        return 1;
+    }
+
+    public boolean checkIsTriangle(Triangle triangle) {
+        return false;
+    }
+
+    public boolean checkIsRectengular(Triangle triangle) {
+        return false;
+    }
 }
