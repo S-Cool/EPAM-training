6df657709fcaef7ff83b50765d8f7a4e94207e49
Ivan <silver2006@tut.by>
2018-06-16 23:40:57 +0300

    refactor(ActionTriangle) Refactor.

--- src/main/java/com/kuliashou/shape/action/ActionTriangle.java
+++ src/main/java/com/kuliashou/shape/action/ActionTriangle.java
@@ -11,12 +11,12 @@ public class ActionTriangle {
 
     private static final Logger logger = LogManager.getLogger();
 
-    public static double calculateArea(Triangle triangle) {
+    public double calculateArea(Triangle triangle) {
 
         double a = ActionTriangle.calculateSideA(triangle);
         double b = ActionTriangle.calculateSideB(triangle);
         double c = ActionTriangle.calculateSideC(triangle);
-        double p = ActionTriangle.calculatePerimeter(triangle) / 2;
+        double p = (a + b + c) / 2;
         double area = sqrt(p * (p - a) * (p - b) * (p - c));
 
         logger.info("Area has calculated!");
@@ -24,7 +24,7 @@ public class ActionTriangle {
         return area;
     }
 
-    public static double calculatePerimeter(Triangle triangle) {
+    public double calculatePerimeter(Triangle triangle) {
 
         double a = ActionTriangle.calculateSideA(triangle);
         double b = ActionTriangle.calculateSideB(triangle);
@@ -36,7 +36,7 @@ public class ActionTriangle {
         return perimeter;
     }
 
-    public static boolean checkIsTriangle(Triangle triangle) {
+    public boolean checkIsTriangle(Triangle triangle) {
         double left = (triangle.getPointA().getX() - triangle.getPointC().getX())
                 / (triangle.getPointB().getX() - triangle.getPointC().getX());
 
@@ -50,12 +50,12 @@ public class ActionTriangle {
         return conclusion;
     }
 
-    public static boolean checkIsRectangular(Triangle triangle) {
+    public boolean checkIsRectangular(Triangle triangle) {
         double a = ActionTriangle.calculateSideA(triangle);
         double b = ActionTriangle.calculateSideB(triangle);
         double c = ActionTriangle.calculateSideC(triangle);
-        boolean conclusion = sqrt(abs(a)) + sqrt(abs(c)) == sqrt(abs(b)) |
-                sqrt(abs(c)) + sqrt(abs(b)) == sqrt(abs(a)) |
+        boolean conclusion = sqrt(abs(a)) + sqrt(abs(c)) == sqrt(abs(b)) ||
+                sqrt(abs(c)) + sqrt(abs(b)) == sqrt(abs(a)) ||
                 sqrt(abs(a)) + sqrt(abs(b)) == sqrt(abs(c));
 
         logger.info("The Figure is verified that it's rectangular!");
@@ -66,21 +66,18 @@ public class ActionTriangle {
     private static double calculateSideA(Triangle triangle) {
         double dx = (triangle.getPointA().getX() - triangle.getPointB().getX());
         double dy = (triangle.getPointA().getY() - triangle.getPointB().getY());
-        double a = Math.hypot(dx, dy);
-        return a;
+        return Math.hypot(dx, dy);
     }
 
     private static double calculateSideB(Triangle triangle) {
         double dx = (triangle.getPointB().getX() - triangle.getPointC().getX());
         double dy = (triangle.getPointB().getY() - triangle.getPointC().getY());
-        double b = Math.hypot(dx, dy);
-        return b;
+        return Math.hypot(dx, dy);
     }
 
     private static double calculateSideC(Triangle triangle) {
         double dx = (triangle.getPointC().getX() - triangle.getPointA().getX());
         double dy = (triangle.getPointC().getY() - triangle.getPointA().getY());
-        double c = Math.hypot(dx, dy);
-        return c;
+        return Math.hypot(dx, dy);
     }
 }
